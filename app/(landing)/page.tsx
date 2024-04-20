ffe6ba598725ab4e855bbdd48ab1ba680cdce680
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2024-04-20 21:24:41 +0530

    login and logout done

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,20 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Link href="/sign-in">
+        <Button>
+          Login
+        </Button>
+      </Link>
+      <Link href="/sign-up">
+        <Button>
+          Register
+        </Button>
+      </Link>
+    </div>
+  )
+}
