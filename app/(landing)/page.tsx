22afd94baf366ed668ff1800c2be1890f77901b5
omkar-ghongade <omkarsubhashghongade21@gmail.com>
2024-04-20 22:26:23 +0530

    added redirect

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,8 +1,17 @@
 import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import React from 'react'
+import { currentUser } from '@clerk/nextjs/server';
+import { redirect } from 'next/navigation'
 
-export default function page() {
+export default async function page() {
+
+  const user = await currentUser();
+
+  if(user) redirect('/dashboard')
+
+
+
   return (
     <div>
       <Link href="/sign-in">
